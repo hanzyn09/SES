d10f85ab0d44ff5a2f643cd0a56e21ec0c976890
DAWUNKIM <beutkid2@naver.com>
2019-11-13 17:44:17 +0900

    SES background

--- ses/src/ses/app.java
+++ ses/src/ses/app.java
@@ -41,13 +41,14 @@ public class app {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		//background
 		frame = new JFrame();
-		ImagePanel welcomepanel = new ImagePanel(new ImageIcon("C:\\ses\\space2.png").getImage());
+		ImagePanel welcomepanel = new ImagePanel(new ImageIcon(this.getClass().getResource("/space2.png")).getImage());
 		frame.setSize(welcomepanel.getWidth(),welcomepanel.getHeight());
 		frame.getContentPane().add(welcomepanel);
 		
 		//planet1~8(mercury~neptune)
-		ImageIcon mercury = new ImageIcon("C:\\ses\\mercury.png");
+		ImageIcon mercury = new ImageIcon(this.getClass().getResource("/mercury.png"));
 		Image mercu = mercury.getImage();
 		Image merc = mercu.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
 		ImageIcon m = new ImageIcon(merc);
@@ -55,7 +56,7 @@ public class app {
 		lblNewLabel.setBounds(220,80,100,100);
 		welcomepanel.add(lblNewLabel);
 		
-		ImageIcon venus = new ImageIcon("C:\\ses\\venus.png");
+		ImageIcon venus = new ImageIcon(this.getClass().getResource("/venus.png"));
 		Image venu = venus.getImage();
 		Image ven = venu.getScaledInstance(110, 110, java.awt.Image.SCALE_SMOOTH);
 		ImageIcon v = new ImageIcon(ven);
@@ -63,7 +64,7 @@ public class app {
 		lblNewLabel2.setBounds(390,80,110,110);
 		welcomepanel.add(lblNewLabel2);
 		
-		ImageIcon earth = new ImageIcon("C:\\ses\\earth.png");
+		ImageIcon earth = new ImageIcon(this.getClass().getResource("/earth.png"));
 		Image eart = earth.getImage();
 		Image ear = eart.getScaledInstance(120, 120, java.awt.Image.SCALE_SMOOTH);
 		ImageIcon e = new ImageIcon(ear);
@@ -71,7 +72,7 @@ public class app {
 		lblNewLabel3.setBounds(590,80,120,120);
 		welcomepanel.add(lblNewLabel3);
 		
-		ImageIcon mars = new ImageIcon("C:\\ses\\mars.png");
+		ImageIcon mars = new ImageIcon(this.getClass().getResource("/mars.png"));
 		Image mar = mars.getImage();
 		Image ma = mar.getScaledInstance(130, 130, java.awt.Image.SCALE_SMOOTH);
 		ImageIcon m2 = new ImageIcon(ma);
@@ -79,7 +80,7 @@ public class app {
 		lblNewLabel4.setBounds(790,80,130,130);
 		welcomepanel.add(lblNewLabel4);
 		
-		ImageIcon jupiter = new ImageIcon("C:\\ses\\jupiter.png");
+		ImageIcon jupiter = new ImageIcon(this.getClass().getResource("/jupiter.png"));
 		Image jupite = jupiter.getImage();
 		Image jupit = jupite.getScaledInstance(160, 160, java.awt.Image.SCALE_SMOOTH);
 		ImageIcon j = new ImageIcon(jupit);
@@ -87,7 +88,7 @@ public class app {
 		lblNewLabel5.setBounds(1000,60,160,160);
 		welcomepanel.add(lblNewLabel5);
 		
-		ImageIcon saturn = new ImageIcon("C:\\ses\\saturn.png");
+		ImageIcon saturn = new ImageIcon(this.getClass().getResource("/saturn.png"));
 		Image satur = saturn.getImage();
 		Image satu = satur.getScaledInstance(140, 140, java.awt.Image.SCALE_SMOOTH);
 		ImageIcon s = new ImageIcon(satu);
@@ -95,7 +96,7 @@ public class app {
 		lblNewLabel6.setBounds(1250,80,140,140);
 		welcomepanel.add(lblNewLabel6);
 		
-		ImageIcon uranus = new ImageIcon("C:\\ses\\uranus.png");
+		ImageIcon uranus = new ImageIcon(this.getClass().getResource("/uranus.png"));
 		Image uranu = uranus.getImage();
 		Image uran = uranu.getScaledInstance(130, 130, java.awt.Image.SCALE_SMOOTH);
 		ImageIcon u = new ImageIcon(uran);
@@ -103,7 +104,7 @@ public class app {
 		lblNewLabel7.setBounds(1450,80,130,130);
 		welcomepanel.add(lblNewLabel7);
 		
-		ImageIcon neptune = new ImageIcon("C:\\ses\\neptune.png");
+		ImageIcon neptune = new ImageIcon(this.getClass().getResource("/neptune.png"));
 		Image neptun = neptune.getImage();
 		Image neptu = neptun.getScaledInstance(130, 130, java.awt.Image.SCALE_SMOOTH);
 		ImageIcon n = new ImageIcon(neptu);
@@ -112,7 +113,7 @@ public class app {
 		welcomepanel.add(lblNewLabel8);
 		
 		//SpaceElevator1
-		ImageIcon spaceship = new ImageIcon("C:\\ses\\spaceship.png");
+		ImageIcon spaceship = new ImageIcon(this.getClass().getResource("/spaceship.png"));
 		Image spaceshi = spaceship.getImage();
 		Image spacesh = spaceshi.getScaledInstance(150, 150, java.awt.Image.SCALE_SMOOTH);
 		ImageIcon s1 = new ImageIcon(spacesh);
