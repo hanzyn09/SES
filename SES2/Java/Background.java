4a0c59bf58a9f2e119904cfbeedadc14af40e66f
KIMDAWUN <33215256+KIMDAWUN@users.noreply.github.com>
2019-11-28 15:28:21 +0900

    Update Background.java

--- SES2/Java/Background.java
+++ SES2/Java/Background.java
@@ -30,7 +30,7 @@ public class Background extends JPanel {
 		/************************Draw background, planet, SES***************************/
 		
 		background =Toolkit.getDefaultToolkit().getImage("image/space2.png");
-		beam =Toolkit.getDefaultToolkit().getImage("image/beam.png");
+		beam =Toolkit.getDefaultToolkit().getImage("image/beam6.png");
 		g.drawImage(background,0,180,1590,800,this);
 		
 		/**********************planet(mercury~)********************/
@@ -45,10 +45,17 @@ public class Background extends JPanel {
 		
 
 		/***********************************Moving SES, stop position-> with beam************************/
-		for (int i=1 ; i<=4 ; i++) {
+		for (int i=1 ; i<=2 ; i++) {
+			g.drawImage(SESControl.getses()[i].getImage(), SESControl.getses()[i].getcurX()+50,SESControl.getses()[i].getcurY(), tempY+50, tempY+50, this);
+			if(SESControl.getses()[i].getSESState().equals("Wait")) {
+				g.drawImage(beam, SESControl.getses()[i].getcurX()+70,180, tempY, SESControl.getses()[i].getcurY()-80, this);
+		}
+		}
+		for (int i=3 ; i<=4 ; i++) {
 			g.drawImage(SESControl.getses()[i].getImage(), SESControl.getses()[i].getcurX(),SESControl.getses()[i].getcurY(), tempY+50, tempY+50, this);
-			if(SESControl.getses()[i].getSESState().equals("Wait"))
-				g.drawImage(beam,SESControl.getses()[i].getcurX(),SESControl.getses()[i].getcurY(), tempY, tempY, this);
+			if(SESControl.getses()[i].getSESState().equals("Wait")) {
+				g.drawImage(beam, SESControl.getses()[i].getcurX()+30,180, tempY, SESControl.getses()[i].getcurY()-80, this);
+		}
 		}
 		
 
